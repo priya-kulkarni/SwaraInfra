f8be9a87be008b6cfc6cea32d3cca82d69046484
priya kulkarni <kulkarnipriya511@gmail.com>
2025-01-12 19:55:39 +0530

    Minor changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,20 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import '../styles/HeroSection.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // Import AOS CSS
+import hero from "../assets/hero1.mp4";
+import hero1 from "../assets/hero2.mp4";
+import hero2 from "../assets/hero3.mp4";
 
 function HeroSection() {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000, // Animation duration
-      easing: 'ease-in-out', // Easing function
-      once: true, // Animation happens once
-    });
-  }, []);
-
   return (
     <div className="hero-section">
       <div id="heroCarousel" className="carousel slide" data-bs-ride="carousel">
@@ -44,34 +37,49 @@ function HeroSection() {
 
         {/* Carousel Items */}
         <div className="carousel-inner">
-          <div className="carousel-item active" data-aos="fade-up">
-            <img
-              src="https://themes247.net/html5/construction-template/demo/assets/img/slider/8.jpg"
+          <div className="carousel-item active">
+            <video 
               className="d-block w-100"
-              alt="Slide 1"
-            />
+              autoPlay
+              loop
+              muted
+              aria-label="Slide 1"
+            >
+              <source src={hero} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
             <div className="carousel-caption d-none d-md-block">
               <h5>We Focus on you</h5>
-              <p>We Provide Expertise and genuine commitment to qualify on every project no matter the size, market, or delivery method.</p>
+              <p>We Provide Expertise and genuine commited to qualify on every project no matter the size, market or delivery method.</p>
             </div>
           </div>
-          <div className="carousel-item" data-aos="fade-up" data-aos-delay="200">
-            <img
-              src="https://themes247.net/html5/construction-template/demo/assets/img/slider/6.jpg"
+          <div className="carousel-item">
+          <video 
               className="d-block w-100"
-              alt="Slide 2"
-            />
+              autoPlay
+              loop
+              muted
+              aria-label="Slide 1"
+            >
+              <source src={hero1} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
             <div className="carousel-caption d-none d-md-block">
               <h5>Project Leadership</h5>
               <p>We will make your building construction Project proceed in a straightforward manner with the least possible impact on your normal operation.</p>
             </div>
           </div>
-          <div className="carousel-item" data-aos="fade-up" data-aos-delay="400">
-            <img
-              src="https://themes247.net/html5/construction-template/demo/assets/img/slider/7.jpg"
+          <div className="carousel-item">
+          <video 
               className="d-block w-100"
-              alt="Slide 3"
-            />
+              autoPlay
+              loop
+              muted
+              aria-label="Slide 1"
+            >
+              <source src={hero2} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
             <div className="carousel-caption d-none d-md-block">
               <h5>Construct Your Dream Home</h5>
               <p>Make your Dream home</p>
